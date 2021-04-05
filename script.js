66fce4f99013afaa70173f38413243e423a6b3ac
Tony Saad <tony.saad@enpal.de>
2021-04-05 18:05:50 +0200

    Replace reloading with innerHTML

--- script.js
+++ script.js
@@ -4,39 +4,34 @@ let quizBody = document.querySelector(".Quiz-body")
 let ul = document.createElement("ul");
 let counter = document.querySelector(".count");
 let result = document.querySelector("#feedback");
-let playerInfo = document.createElement("div");
-let clearscore = document.querySelector(".clear-score");
 let scoreSheet = document.querySelector("#Highscore");
 let time = 40;
 let count = 0;
 let score = 0;
 let endQuiz = false;
 let participants = JSON.parse(localStorage.getItem("scoresheet")) || [];
+
 function setTitle(title) {
     document.querySelector(".js-title").textContent = title
 }
 
-
 function startTimer() {
-
-    let timer = setInterval(function () {
-
-        if (time <= 0 || endQuiz) {
-            counter.textContent = 0;
-            clearInterval(timer);
-            checkScore();
-            addToScoreSheet();
-            return
-        }
-        counter.textContent = time;
-        time--;
-    },
+    let timer = setInterval(function() {
+            if (time <= 0 || endQuiz) {
+                counter.textContent = 0;
+                clearInterval(timer);
+                checkScore();
+                addToScoreSheet();
+                return
+            }
+            counter.textContent = time;
+            time--;
+        },
         1000);
 
 }
 
 function startQuiz() {
-
     startTimer();
     renderQuestionAndAnswer(0);
 
@@ -74,16 +69,8 @@ function renderQuestionAndAnswer(i) {
     setTitle(selectedQuestion.question);
     var choiselist = selectedQuestion.choices;
     for (var index = 0; index < choiselist.length; index++) {
-        // let li = document.createElement("li");
-        // li.setAttribute("data-index", index);
-        // button = document.createElement("button");
-        // button.textContent = choiselist[index];
-        // li.appendChild(button);
-        // answers.appendChild(ul);
-        // ul.appendChild(li);
-        //<button>${choiselist[index].replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')}</button>
         let li = `<li data-index="${index}">
-            <button>${choiselist[index]}</button>
+            <button>${choiselist[index].replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')}</button>
         </li>`
         ul.insertAdjacentHTML('beforeend', li);
     }
@@ -101,18 +88,11 @@ function renderResult(message) {
 
 function addToScoreSheet() {
     //template litterals
-    answers = "";
-    let input = document.createElement("input");
-    let submitButton = document.createElement("button");
-    input.setAttribute("placeholder", "Enter intials");
-    input.setAttribute("type", "text");
-    input.setAttribute("id", "name");
-    playerInfo.setAttribute("id", "Highscore");
-    submitButton.setAttribute("id", "submit-button");
-    submitButton.textContent = "Add to Score Sheet";
-    quizBody.appendChild(playerInfo);
-    playerInfo.appendChild(input);
-    playerInfo.appendChild(submitButton);
+    let playerInfo = `<div id="Highscore">
+        <input type="text" id="name" placeholder="Enter initials"/>
+        <button id="submit-button">Add to Score Sheet</button>
+    </div>`;
+    quizBody.insertAdjacentHTML('beforeend', playerInfo)
 
 
 }
@@ -150,25 +130,35 @@ quizBody.addEventListener("click", (e) => {
         var name = document.querySelector("#name").value;
         localStorage.setItem("name", name);
         participants.push({
-            name,//same as `name: name`
+            name, //same as `name: name`
             score: localStorage.getItem("score")
         });
         // to be disscused
         localStorage.setItem("scoresheet", JSON.stringify(participants));
         showScoresheet()
+    } else if (e.target.matches('.js-clear')) {
+        localStorage.clear('scoresheet')
+        document.querySelector('.records').innerHTML = ''
+    } else if (e.target.matches('.js-reload')) {
+        answers.innerHTML = `<p>Press the button below to start the game. Incorrect answers incur a time penalty!</p>
+        <button class="start-button ">Start Quiz</button>`
     }
 });
 
 function showScoresheet() {
-    var records = document.createElement("ul");
-    for (var x = 0; x < participants.length; x++) {
-        var playerRecord = document.createElement("li");
-        playerRecord.innerHTML = `${participants[x].name} <strong>${participants[x].score}</strong>`;
-        records.appendChild(playerRecord);
-    }
     setTitle("Score Sheet");
-    quizBody.textContent = "";
+    var records = document.createElement("ul");
+    records.setAttribute('class', 'records')
+    participants.forEach((p) => records.insertAdjacentHTML('beforeend', `<li>${p.name} <strong>${p.score}</strong></li>`))
     quizBody.appendChild(records);
+    quizBody.insertAdjacentHTML('beforeend', `<button class="js-clear">Clear Score</button>`)
+    quizBody.insertAdjacentHTML('beforeend', `<button class="js-reload">Play Again ?</button>`)
+        // for (var x = 0; x < participants.length; x++) {
+        // var playerRecord = document.createElement("li");
+        // playerRecord.innerHTML = `${participants[x].name} <strong>${participants[x].score}</strong>`;
+        // records.appendChild(playerRecord);
+        // }
 }
 
+document.querySelector('.js-showscore').addEventListener('click', showScoresheet)
 document.querySelector('.js-showscore').addEventListener('click', showScoresheet)
